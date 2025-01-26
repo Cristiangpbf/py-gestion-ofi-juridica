505e4ff12178ad63e14a16cec677438e04cf5668
Cristian <cristian.guamba>
2025-01-25 23:19:38 -0500

    Primer commit

---
+++ juridic/src/main/java/com/uisrael/juridic/JuridicApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.juridic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuridicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuridicApplication.class, args);
+	}
+
+}
---
+++ juridic/src/test/java/com/uisrael/juridic/JuridicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.juridic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuridicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
